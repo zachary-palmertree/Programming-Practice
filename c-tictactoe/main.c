a4bbdb5b8bcf4ffa33534465e655c92695b455f8
zachary-palmertree <zachary.palmertree@outlook.com>
2024-11-27 22:06:05 -0500

    added for loop demonstration

--- c-tictactoe/main.c
+++ c-tictactoe/main.c
@@ -1,7 +1,11 @@
 # include <stdio.h>
 
 int main() {
-    printf("Hello world!");
+    printf("Hello world!\n");
+
+    for(int x=1; x<10; x++) {
+        printf("%d\n", x); // This should print the first/only variable x as an integer        
+    }
 
     return 0;
 }
